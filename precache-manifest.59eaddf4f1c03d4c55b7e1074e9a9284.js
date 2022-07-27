b0bac4acef03cb2ee6d5c609f0074dc40b04a54e
OlegYurov <96524921+OlegYurov@users.noreply.github.com>
2022-07-27 09:05:34 +0300

    Updates

---
+++ precache-manifest.59eaddf4f1c03d4c55b7e1074e9a9284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "100c460e2c8a697b2878",
+    "url": "/react-hw-phonebook/static/css/main.15ccd999.chunk.css"
+  },
+  {
+    "revision": "100c460e2c8a697b2878",
+    "url": "/react-hw-phonebook/static/js/main.100c460e.chunk.js"
+  },
+  {
+    "revision": "c57be8183b9577897f2f",
+    "url": "/react-hw-phonebook/static/js/1.c57be818.chunk.js"
+  },
+  {
+    "revision": "8bfe96cc8ac6ced4d4f9",
+    "url": "/react-hw-phonebook/static/js/runtime~main.8bfe96cc.js"
+  },
+  {
+    "revision": "944b9b8f196a6f08459134c6945c167d",
+    "url": "/react-hw-phonebook/index.html"
+  }
+];
